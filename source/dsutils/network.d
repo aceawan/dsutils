4a038fe91e08bde61f198bf053b558442d60b4ca
aceawan <aceawan@acelaptop>
2015-06-24 13:58:00 +0200

    add a network io stats per interface function

--- source/dsutils/network.d
+++ source/dsutils/network.d
@@ -27,3 +27,27 @@ NetIoStats netIoCounters(){
 
 	return result;
 }
+
+NetIoStats[string] netIoCountersPerNic(){
+	auto lines = readText("/proc/net/dev");
+	NetIoStats[string] result;
+
+	foreach(line; lines.splitLines()[2..$]){
+		auto tmp_tuple = NetIoStats();
+
+		auto values = line.squeeze(" ").split(":")[1].split();
+
+		tmp_tuple.bytes_recvd += values[0].to!int;
+		tmp_tuple.packets_recvd += values[1].to!int;
+		tmp_tuple.errin += values[2].to!int;
+		tmp_tuple.dropin += values[3].to!int;
+		tmp_tuple.bytes_sent += values[8].to!int;
+		tmp_tuple.packets_sent += values[9].to!int;
+		tmp_tuple.errout += values[10].to!int;
+		tmp_tuple.dropout += values[11].to!int;
+
+		result[line.split(":")[0].strip] = tmp_tuple;
+	}
+
+	return result;
+}
